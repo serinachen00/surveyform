753e8eefe1bbe796511e060a6575af7db0063110
serinachen00 <serina2serinaeva@gmail.com>
2020-07-15 18:12:15 -0700

    First commit

---
+++ newmod/data.js
@@ -0,0 +1,42 @@
+
+
+$(document).ready(function()  {
+
+
+ $('#submit').click((event)=> {
+
+    //stop submit the form, we will post it manually.
+    event.preventDefault();
+    doAjax();
+
+});
+
+
+
+});
+
+function doAjax() {
+
+    var fname = $('#first').val();
+    console.log("1st name"+ fname)
+    var lname = $('#last').val();
+    console.log("lst name"+ lname);
+
+    window.sessionStorage.setItem('firstname', fname);
+    window.sessionStorage.setItem('lastname', lname);
+
+    console.log(sessionStorage.getItem('firstname'))
+    console.log(sessionStorage.getItem('lastname'))
+
+
+    if(fname != '' && lname !=''){
+    //if($('input:required') != ''){
+
+    window.location.href= '../newmod/dataresult.html';
+    }
+
+    else {
+        alert("please input your field");
+    }
+}
+
