d927ce74b966f749b7740810b35808ed6b1367a1
serinachen00 <serina2serinaeva@gmail.com>
2020-07-16 20:55:44 -0700

    survery form: added more input and functionality

--- newmod/data.js
+++ newmod/data.js
@@ -15,28 +15,64 @@ $(document).ready(function()  {
 
 });
 
+
+
 function doAjax() {
 
+    //var output = document.getElementById('userinput');
+
     var fname = $('#first').val();
-    console.log("1st name"+ fname)
     var lname = $('#last').val();
-    console.log("lst name"+ lname);
+    var dobb = $('#dobb').val();
+    var  hftt= $('#hftt').val();
+    var hii = $('#hii').val();
+    var eduu = $('#inputState').val();
+    var phh = $('#phone').val();
+    var emill = $('#email').val();
+    var add11 = $('#address1').val();
+    var add22 = $('#address2').val();
+    var cityy = $('#city').val();
+    var statee = $('#state').val();
+    var zipp = $('#zipcode').val();
 
     window.sessionStorage.setItem('firstname', fname);
     window.sessionStorage.setItem('lastname', lname);
+    window.sessionStorage.setItem('dob', dobb);
+    window.sessionStorage.setItem('hft', hftt);
+    window.sessionStorage.setItem('hi', hii);
+    window.sessionStorage.setItem('edu', eduu);
+    window.sessionStorage.setItem('ph', phh);
+    window.sessionStorage.setItem('emil', emill);
+    window.sessionStorage.setItem('add1', add11);
+    window.sessionStorage.setItem('add2', add22);
+    window.sessionStorage.setItem('city', cityy);
+    window.sessionStorage.setItem('state', statee);
+    window.sessionStorage.setItem('zip', zipp);
 
-    console.log(sessionStorage.getItem('firstname'))
-    console.log(sessionStorage.getItem('lastname'))
 
 
     if(fname != '' && lname !=''){
     //if($('input:required') != ''){
+     window.location.href= 'newmod/dataresult.html';
 
-    window.location.href= 'newmod/dataresult.html';
+      // display();
+
     }
 
     else {
         alert("please input your field");
     }
+
+   // output.innerHTML = htmlstring;
+   /* $('#userinput').html('First Name: '+ sessionStorage.getItem('firstname'));
+    $('#userinput').html('Last Name: '+ sessionStorage.getItem('lastname') ); */
+
+
 }
 
+/*function display() {
+
+    console.log("lst name"+ lname);
+    document.getElementById('userinput').innerHTML = 'yikesssss' ;
+
+}*/
